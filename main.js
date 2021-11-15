6e37357f5b9a9ac47e34453b4b898903acb461e0
bracq <florianbracq42@gmail.com>
2021-11-15 14:27:07 +0100

    Adding the basic configuration

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "/src/preload.js")
+        }
+    })
+
+    win.loadFile('index.html')
+
+    win.webContents.openDevTools()
+
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// Managing the closing of all windows 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
