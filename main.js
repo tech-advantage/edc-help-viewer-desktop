6b7fd80d5fe5eb68a931e410ecc616730c0e2f31
bracq <florianbracq42@gmail.com>
2021-11-25 16:48:26 +0100

    unzip folder and move directory to help

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const ROOT_FOLDER = path.resolve(__dirname)
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
@@ -10,21 +11,23 @@ function createWindow () {
         }
     })
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
 
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
 
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
 
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
     })
 })
 
-// Managing the closing of all windows 
+// Managing the closing of all windows
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit();
 })
+
+fs.rmSync(ROOT_FOLDER + "\\" + 'edc-help-viewer.3.2.2', { recursive: true, force: true }, console.error);
