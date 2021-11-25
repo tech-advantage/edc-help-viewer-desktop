04514ff0260770b38a3bcde281dd3907682e8575
bracq <florianbracq42@gmail.com>
2021-11-25 16:48:26 +0100

    unzip folder and move directory to help

--- init_help_viewer.js
+++ init_help_viewer.js
@@ -1,20 +1,20 @@
 
-const path = require('path')
-const fs = require('fs')
-const fse = require('fs-extra');
-const request = require('superagent')
+const path = require('path');
+const fs = require('fs');
+var fsExtra = require('fs-extra');
+const request = require('superagent');
 const admZip = require('adm-zip');
-const glob = require('glob')
 
-const ROOT_FOLDER = path.resolve(__dirname)
-const REPO_NAME = 'edc-help-viewer'
-const ZIP_FILE = 'edc-help-viewer.3.2.2.zip'
+const ROOT_FOLDER = path.resolve(__dirname);
+const REPO_NAME = 'edc-help-viewer';
+const ZIP_FILE = 'edc-help-viewer.3.2.2.zip';
+const EDC_VIEWER_FOLDER = 'edc-help-viewer.3.2.2';
 const GIT_EDC_VIEWER_URL = `https://github.com/tech-advantage/${REPO_NAME}/releases/download/v3.2.2`;
 const source = `${GIT_EDC_VIEWER_URL}/${ZIP_FILE}`;
 
-const srcDir = `${ROOT_FOLDER}\\edc-help-viewer.3.2.2`;
-const destDir = `${ROOT_FOLDER}\\toto`;
-console.log('srcDir', srcDir)
+const srcDir = `${ROOT_FOLDER}\\${EDC_VIEWER_FOLDER}\\help`;
+const destDir = `${ROOT_FOLDER}\\help`;
+
 module.exports.initViewer = function () {
     request
     .get(source)
@@ -22,7 +22,7 @@ module.exports.initViewer = function () {
         console.log(error);
     })
     .pipe(fs.createWriteStream(ZIP_FILE))
-    .on('finish', function() {
+    .on('finish', async function() {
         console.log('finished dowloading');
         let zip = new admZip(ZIP_FILE);
         console.log('start unzip');
@@ -30,5 +30,10 @@ module.exports.initViewer = function () {
         zip.extractAllTo(ROOT_FOLDER, true);
         console.log('finished unzip');
         
+        await fsExtra.move(srcDir, destDir, console.error);
+        
+        fs.unlinkSync(ROOT_FOLDER + "\\" + ZIP_FILE);
+        //fs.rmdirSync(ROOT_FOLDER + "\\" + 'edc-help-viewer.3.2.2');
+        //fs.rmSync(ROOT_FOLDER + "\\" + 'edc-help-viewer.3.2.2', { recursive: true, force: true }, console.error)
     });
 };
