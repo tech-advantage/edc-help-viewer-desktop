94671cfb8938ac4c4f03e5fe25c9fc315a010d39
bracq <florianbracq42@gmail.com>
2021-11-26 10:39:57 +0100

    Copy, move and remove unzip directory

--- src/utils/init_help_viewer.js
+++ src/utils/init_help_viewer.js
@@ -1,17 +1,14 @@
-
-const path = require('path');
 const fs = require('fs');
 var fsExtra = require('fs-extra');
 const request = require('superagent');
 const admZip = require('adm-zip');
 
-const ROOT_FOLDER = path.resolve(__dirname);
+const ROOT_FOLDER = process.cwd();
 const REPO_NAME = 'edc-help-viewer';
 const ZIP_FILE = 'edc-help-viewer.3.2.2.zip';
 const EDC_VIEWER_FOLDER = 'edc-help-viewer.3.2.2';
 const GIT_EDC_VIEWER_URL = `https://github.com/tech-advantage/${REPO_NAME}/releases/download/v3.2.2`;
 const source = `${GIT_EDC_VIEWER_URL}/${ZIP_FILE}`;
-
 const srcDir = `${ROOT_FOLDER}\\${EDC_VIEWER_FOLDER}\\help`;
 const destDir = `${ROOT_FOLDER}\\help`;
 
@@ -30,10 +27,15 @@ module.exports.initViewer = function () {
         zip.extractAllTo(ROOT_FOLDER, true);
         console.log('finished unzip');
         
-        await fsExtra.move(srcDir, destDir, console.error);
+        fsExtra.move(srcDir, destDir, (err) =>{
+            if(!err){
+                console.log('The folder ' + EDC_VIEWER_FOLDER + ' was removed.')
+                fs.rmSync(ROOT_FOLDER + "\\" + EDC_VIEWER_FOLDER, { recursive: true, force: true }, console.error);
+            }else {
+                console.error
+            }
+        })
         
         fs.unlinkSync(ROOT_FOLDER + "\\" + ZIP_FILE);
-        //fs.rmdirSync(ROOT_FOLDER + "\\" + 'edc-help-viewer.3.2.2');
-        //fs.rmSync(ROOT_FOLDER + "\\" + 'edc-help-viewer.3.2.2', { recursive: true, force: true }, console.error)
-    });
+    })
 };
