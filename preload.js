c17f3cd7c71ed36b9cf8da16acc9c70cf166d0a6
bracq <florianbracq42@gmail.com>
2022-05-04 10:38:12 +0200

    Set base href and css file to the index.html after emit post request.

--- preload.js
+++ preload.js
@@ -1,13 +1,13 @@
 require('./src/server.js');
 const configViewer = require('./conf/config_electron_viewer.json');
+const path = require('path');
 var fs = require('fs');
 
-  
 window.addEventListener('DOMContentLoaded', () => {
 
   let imgContent = document.querySelector('.img-content');
   let imgViewer = document.querySelector('.img-content img');
-  
+
   if(imgContent == null){
 
     var scriptRuntimeEs2015 = document.createElement('script');
@@ -35,11 +35,34 @@ window.addEventListener('DOMContentLoaded', () => {
     scriptMainEs2015.type = 'module';
     scriptMainEs2015.async;
 
+    let rootElemBase = document.querySelector('app-root');
+
+    if(!rootElemBase){
+      var base = document.createElement('base');
+      base.href = '/help/';
+      
+      document.getElementsByTagName('head')[0].appendChild(base);
+      var head = document.getElementsByTagName('head')[0];
+      let cssFiles = fs.readdirSync(path.join(__dirname, './static/help/assets/style'))
+      
+      for(let file of cssFiles){
+        var link  = document.createElement('link');
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = `http://localhost:60000/help/assets/style/${file}`;
+        head.appendChild(link);
+      }
+
+      let elem = document.createElement('app-root')
+      document.body.prepend(elem);
+    }
+  
     document.body.appendChild(scriptRuntimeEs2015);
     document.body.appendChild(scriptPolyfillsEs2015);
     document.body.appendChild(scriptStylesEs2015);
     document.body.appendChild(scriptVendorEs2015);
     document.body.appendChild(scriptMainEs2015);
+
   } else {
     imgViewer.src = configViewer.img_loader;
   }
