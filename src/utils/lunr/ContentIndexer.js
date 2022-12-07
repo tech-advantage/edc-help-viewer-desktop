624c796fe68cdc7d5d15787c96e0198bd2f07676
bracq <florianbracq42@gmail.com>
2022-12-07 11:33:11 +0100

    Rewritting content indexer
    Rewritting content indexer to manage more than one product from doc directory

--- src/utils/lunr/ContentIndexer.js
+++ src/utils/lunr/ContentIndexer.js
@@ -1,14 +1,13 @@
-const homeDirectory = require('os').homedir()
 const path = require('path')
 const fs = require('fs')
 const lunr = require('lunr')
-
 const htmlFormatter = require('../HtmlFormatter')
 const Logger = require('../../lib/Logger')
+const PathResolver = require('../PathResolver')
 
 class ContentIndexer {
   static docBasePath = '../../../static/doc'
-  static multiDocItems = {}
+  static multiDocItems = []
   static documents = []
 
   /**
@@ -18,9 +17,13 @@ class ContentIndexer {
      */
   static getMultiDocContent () {
     const multidoc = require(path.join(__dirname, ContentIndexer.docBasePath + '/multi-doc.json'))
-    ContentIndexer.multiDocItems.productId = multidoc[0].productId
-    ContentIndexer.multiDocItems.pluginId = multidoc[0].pluginId
-
+    
+    for(const doc of multidoc){
+      let docElement = {}
+      docElement.productId = doc.productId
+      docElement.pluginId = doc.pluginId
+      ContentIndexer.multiDocItems.push(docElement)
+    }
     return ContentIndexer.multiDocItems
   }
 
@@ -28,14 +31,27 @@ class ContentIndexer {
      * Set toc reference
      */
   static tocIndexer () {
-    const productFolder = path.join(__dirname, ContentIndexer.docBasePath + '/' + ContentIndexer.getMultiDocContent().pluginId)
-    const tocJsonFile = require(productFolder + '/toc.json')
-    const allTocsFiles = []
-    for (const toc of tocJsonFile.toc) {
-      allTocsFiles.push(toc)
+    let productsDirPath = []
+    let productsData = []
+
+    for(const multidoc of ContentIndexer.getMultiDocContent()){
+      productsDirPath.push(path.join(__dirname, ContentIndexer.docBasePath + '/' + multidoc.pluginId))
     }
-    for (const toc of allTocsFiles) {
-      ContentIndexer.indexTocReference(productFolder, toc)
+    for(const productDirPath of productsDirPath){
+      let productItemObject = {}
+      productItemObject.productDirPath = productDirPath
+      try{
+        productItemObject.item = require(productDirPath + '/toc.json')
+      }
+      catch (e) {
+        Logger.log().error("Toc files for the given product not found", e)
+      }
+      productsData.push(productItemObject)
+    }
+    for(const product of productsData){
+      for(const toc of product.item.toc){
+        ContentIndexer.indexTocReference(product.productDirPath, toc)
+      }
     }
   }
 
@@ -58,7 +74,7 @@ class ContentIndexer {
         strategyId,
         strategyLabel,
         type: toc.type,
-        languageCode,
+        languageCode: languageCode,
         content_lower: htmlFormatter.removeTags(htmlFormatter.splitHtml(toc)).toLowerCase(),
         content: htmlFormatter.removeTags(htmlFormatter.splitHtml(toc))
       }
@@ -74,11 +90,11 @@ class ContentIndexer {
   /**
      * Set the content of topics nodes
      *
-     * @param {*} productFolder
+     * @param {*} productDir
      * @param {*} tocReference
      */
-  static indexTocReference (productFolder, tocReference) {
-    const tocXjsonFile = productFolder + '\\' + tocReference.file
+  static indexTocReference (productDir, tocReference) {
+    const tocXjsonFile = productDir + '\\' + tocReference.file
 
     if (tocXjsonFile) {
       let languageCode
@@ -102,58 +118,24 @@ class ContentIndexer {
      * Create and write the index with lunr
      */
   static createIndex () {
-    const customTokenizerBackup = function (obj) {
-      const arr = []
-      const str = obj.toString()
-      const strLength = str.length
-      let originalToken
-      const tokens = []
-      let index
-      let endIndex
-      let isCompoundWord
-      console.log(str, 'strstr')
-      arr.push(str.split(' '))
-
-      for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-          index = str.indexOf(arr[i][j])
-          if (index !== -1) {
-            endIndex = index + arr[i][j].length - 1
-            console.log(index, endIndex)
-          }
-
-          tokens.push(
-            new lunr.Token(arr[i][j], {
-              position: [index, index + arr[i][j].length - 1],
-              index: tokens.length,
-              originalToken: arr[i][j]
-            })
-          )
-        }
-      }
-
-      return tokens
-    }
-
+    
     const pagesIndex = ContentIndexer.documents
     const lunrIndex = lunr(function () {
-      // this.tokenizer = customTokenizerBackup;
-
-      this.pipeline.remove(lunr.stopWordFilter)
-      this.pipeline.remove(lunr.stemmer)
-
-      this.field('label')
-      this.field('content')
-      // this.field('content_lower')
-      this.ref('id')
-      this.metadataWhitelist = ['position']
-      pagesIndex.forEach(function (doc) {
-        this.add(doc)
+        
+    this.pipeline.remove(lunr.stopWordFilter)
+    this.pipeline.remove(lunr.stemmer)
+      
+    this.field('label')
+    this.field('content')
+    this.ref('id')
+    this.metadataWhitelist = ['position']
+    pagesIndex.forEach(function (doc) {
+      this.add(doc)
       }, this)
     })
 
     const indexedContent = JSON.stringify(lunrIndex, null, 4)
-    fs.writeFileSync(homeDirectory + '/edc_help_viewer/index/lunr.json', indexedContent)
+    fs.writeFileSync(PathResolver.getUserHome() + '/edc_help_viewer/index/lunr.json', indexedContent)
   }
 }
 module.exports = ContentIndexer
