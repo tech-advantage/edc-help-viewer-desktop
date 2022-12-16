bdc76d0d350b66fc9ba9f29935697fec6ca3217c
bracq <florianbracq42@gmail.com>
2022-12-16 14:16:28 +0100

    udpate config

--- src/utils/ConfigElectronViewer.js
+++ src/utils/ConfigElectronViewer.js
@@ -1,79 +1,107 @@
-const configViewer = require('../../conf/config_electron_viewer.json')
+const configViewer = require("../../conf/config_electron_viewer.json");
 
 class ConfigElectronViewer {
-  /**
-     * Check the string passed
-     *
-     * @param {*} str
-     * @returns
-     */
-  static isEmpty(str) {
-    return (!str || str.length === 0)
-  }
+	/**
+	 * Check the string passed
+	 *
+	 * @param {*} str
+	 * @returns
+	 */
+	static isEmpty(str) {
+		return !str || str.length === 0;
+	}
 
-  /**
-     * Return the host name
-     *
-     * @returns {string}
-     */
-  static getHostName() {
-    if (this.isEmpty(configViewer.hostname)) { return 'http://localhost' }
-    return configViewer.hostname
-  }
+	/**
+	 * Return the host name
+	 *
+	 * @returns {string}
+	 */
+	static getHostName() {
+		if (this.isEmpty(configViewer.hostname)) {
+			return "http://localhost";
+		}
+		return configViewer.hostname;
+	}
 
-  /**
-     * Return the server port number
-     *
-     * @returns {number}
-     */
-  static getServerPort() {
-    if (!configViewer.server_port) { return 60000 }
-    if (typeof configViewer.server_port === 'string') { return parseInt(configViewer.server_port) }
+	/**
+	 * Return the server port number
+	 *
+	 * @returns {number}
+	 */
+	static getServerPort() {
+		if (!configViewer.server_port) {
+			return 60000;
+		}
+		if (typeof configViewer.server_port === "string") {
+			return parseInt(configViewer.server_port);
+		}
 
-    return configViewer.server_port
-  }
+		return configViewer.server_port;
+	}
 
-  /**
-   * Return the doc path
-   * 
-   * @returns {string}
-   */
-  static getDocPath() {
-    if (this.isEmpty(configViewer.docPath) && ConfigElectronViewer.isEmbeddedDoc()) {
-      return '/static/doc'
-    }
-    if(process.argv[2] == 'test'){
-      return '/test/ressources/doc'
-    }
-    return configViewer.docPath
-  }
+	/**
+	 * Return the doc path
+	 *
+	 * @returns {string}
+	 */
+	static getDocPath() {
+		configViewer.docPath = "../../../static/doc";
+		if (
+			this.isEmpty(configViewer.docPath) &&
+			ConfigElectronViewer.isEmbeddedDoc()
+		) {
+			configViewer.docPath = "../../../static/doc";
+		}
+		if (process.argv[2] == "test") {
+			configViewer.docPath = "../../../test/ressources/doc";
+		}
+		return configViewer.docPath;
+	}
 
-  /**
-     * Return true if enable
-     *
-     * @returns {boolean}
-     */
-   static isEmbeddedDoc() {
-    return !!configViewer.isEmbeddedDoc
-  }
+	/**
+	 * Return true if enable
+	 *
+	 * @returns {boolean}
+	 */
+	static isEmbeddedDoc() {
+		return !!configViewer.isEmbeddedDoc;
+	}
 
-  /**
-     * Return true if enable
-     *
-     * @returns {boolean}
-     */
-  static isEnableMenu() {
-    return !!configViewer.isEnableMenu
-  }
+	/**
+	 * Return true if enable
+	 *
+	 * @returns {boolean}
+	 */
+	static isEnableMenu() {
+		return !!configViewer.browserWindow.isEnableMenu;
+	}
 
-  /**
-     * Return the viewer img loader
-     *
-     * @returns {string} the viewer img loader
-     */
-  static getImgLoader() {
-    return configViewer.img_loader
-  }
+	/**
+	 * Return the viewer img loader
+	 *
+	 * @returns {string} the viewer img loader
+	 */
+	static getImgLoader() {
+		return configViewer.img_loader;
+	}
+
+	/**
+	 * The width of browser window
+	 *
+	 * @returns browserWindow width
+	 */
+	static getBrowserWindowWidth() {
+		return configViewer.browserWindow.width;
+	}
+
+	/**
+	 * The height of browser window
+	 *
+	 * @returns browserWindow height
+	 */
+	static getBrowserWindowHeight() {
+		return configViewer.browserWindow.height;
+	}
 }
 
-module.exports = ConfigElectronViewer
+module.exports = ConfigElectronViewer;
--- webpack/unzip_help_viewer.js
+++ webpack/unzip_help_viewer.js
@@ -1,28 +1,26 @@
-const fs = require('fs');
-const admZip = require('adm-zip');
-const { ROOT_FOLDER } = require('../conf/edc_const');
-const path = require('path');
-const Logger = require('../src/lib/Logger');
+const fs = require("fs");
+const admZip = require("adm-zip");
+const { ROOT_FOLDER } = require("../conf/edc_const");
+const path = require("path");
+const Logger = require("../src/lib/Logger");
 
 module.exports.unzipViewer = function () {
-    // Read the content of a given directory
-    let zip_file = fs.readdirSync(ROOT_FOLDER + '/dist/zip');
+	// Read the content of a given directory
+	let zip_file = fs.readdirSync(ROOT_FOLDER + "/dist/zip");
 
-    // Read the archive
-    let zip = new admZip(ROOT_FOLDER + '/dist/zip/' + zip_file);
+	// Read the archive
+	let zip = new admZip(ROOT_FOLDER + "/dist/zip/" + zip_file);
 
-    Logger.log().info('%c start unzip', 'color: green');
-    
-    try {
-        // Return an array with files and directory from archive
-        zipEntries = zip.getEntries();
+	Logger.log().info("%c start unzip", "color: green");
 
-        // Extracts the entire archive to the given location.
-        zip.extractAllTo(ROOT_FOLDER, true);
-        Logger.log().info('%c done unzip', 'color: green');
-        
-    } catch (e) {
-        Logger.log().error(e);
-        
-    }
+	try {
+		// Return an array with files and directory from archive
+		zipEntries = zip.getEntries();
+
+		// Extracts the entire archive to the given location.
+		zip.extractAllTo(ROOT_FOLDER, true);
+		Logger.log().info("%c done unzip", "color: green");
+	} catch (e) {
+		Logger.log().error(e);
+	}
 };
